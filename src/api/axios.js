393608d7b1fc1ce1803fa32799308fd17cea951b
Abhay Singh <abhaysingh@MacBook-Air.local>
2026-04-25 21:39:28 +0530

    fixed axios baseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://social-media-server-gidr.onrender.com",
+    baseURL: "https://social-media-server-gidr.onrender.com/api",
     withCredentials: true,
 });
 
